e8971bad5cfc14c0253e2137b809dd0705233a30
kenariy <kenariy@protonmail.com>
2018-07-18 21:42:18 +0300

    posideli i popisali js tak skazat'

---
+++ Participants/Kennadiy/js/script.js
@@ -0,0 +1,23 @@
+'strict-mode';
+
+'kjhnjuk   kj kkk      lkjl'
+
+14.88;
+
+alert('yo yo yo');alert('hi');alert('1488');
+
+{
+    alert('yo yo\n \\ yo');alert('hey');
+    alert('yo yo yo');alert('hi');
+    alert('yo yo yo');alert('hi');
+}
+
+if (2>0) alert('soglasen')
+
+else alert('no');
+
+
+alert(5 / u);
+
+alert(5);
+
