fe3d0e93abdc0d7e8b092809e0c7537aff875388
kenariy <kenariy@protonmail.com>
2018-07-20 21:34:38 +0300

    js игры

--- Participants/Kennadiy/js/script.js
+++ Participants/Kennadiy/js/script.js
@@ -1,6 +1,99 @@
 'strict-mode';
 
-'kjhnjuk   kj kkk      lkjl'
+const month_of_birth = +prompt('month of birth');
+const day_of_birth = +prompt('day of birth');
+
+if ((day_of_birth >=1 && day_of_birth<=21 && month_of_birth ==3) || (day_of_birth<=20 && month_of_birth==4)) { alert('Oven');
+    
+}
+
+else if ((day_of_birth>=21 && day_of_birth<=30 && month_of_birth ==4) || (day_of_birth<=20 && month_of_birth==5)) { alert('Telec');
+    
+}
+
+else if ((day_of_birth >=21 && day_of_birth<=31 && month_of_birth ==5) || (day_of_birth<=21 && month_of_birth==6)) { alert('Blizneci');
+    
+}
+
+else if ((day_of_birth >=22 && day_of_birth<=30 && month_of_birth ==6) || (day_of_birth<=22 && month_of_birth==7)) { alert('Rak');
+    
+}
+
+else if ((day_of_birth >=23 && day_of_birth<=31 && month_of_birth ==7) || (day_of_birth<=23 && month_of_birth==8)) { alert('Lev');
+    
+}
+
+else if ((day_of_birth >=24 && day_of_birth<=30 && month_of_birth ==8) || (day_of_birth<=23 && month_of_birth==9)) { alert('Deva');
+    
+}
+
+else if ((day_of_birth >=24 && day_of_birth<=31 && month_of_birth ==9) || (day_of_birth<=23 && month_of_birth==10)) { alert('Vesi');
+    
+}
+
+else if ((day_of_birth >=24 && day_of_birth<=30 && month_of_birth ==10) || (day_of_birth<=22 && month_of_birth==11)) { alert('Scorpion');
+    
+}
+
+else if ((day_of_birth >=23 && day_of_birth<=31 && month_of_birth ==11) || (day_of_birth<=21 && month_of_birth==12)) { alert('Strelec');
+    
+}
+
+else if ((day_of_birth >=22 && day_of_birth<=30 && month_of_birth ==12) || (day_of_birth<=20 && month_of_birth==1)) { alert('Kozerog');
+    
+}
+
+else if ((day_of_birth >=22 && day_of_birth<=31 && month_of_birth ==1) || (day_of_birth<=23 && month_of_birth==2)) { alert('Vodolei');
+    
+}
+
+else if ((day_of_birth >=21 && day_of_birth<=30 && month_of_birth ==2) || (day_of_birth<=20 && month_of_birth==3)) { alert('Ribi');
+    
+}
+
+else {alert('is none');}
+
+/*const body_temperature = +prompt('temperature?');
+
+
+if (body_temperature >=35 && body_temperature <37) {
+    alert('ok');
+} else if (body_temperature >=37 && body_temperature<40) { alert('dela plohi');}
+
+else if (body_temperature >40) {alert('trup');}
+
+
+else {alert('trup');}
+*/
+
+
+
+
+//const age = prompt('vozrast?', 'placeholder');
+
+//alert('Vam ${age} ');
+
+/*const age = +prompt('vozrast?');
+
+
+if (age >= 18) {
+    alert('yo');
+} else if (age >=7) { alert('rasti');}
+
+else if (age >=3) {alert('v sad');}
+
+
+else {alert('domoi, ocherednyara');}*/
+
+
+
+/* let adultAge = 18;
+
+const isAdult = confirm(`Vam est ${adultAge}?`);
+
+alert(isAdult);*/
+
+/*'kjhnjuk   kj kkk      lkjl'
 
 14.88;
 
@@ -15,9 +108,16 @@ alert('yo yo yo');alert('hi');alert('1488');
 if (2>0) alert('soglasen')
 
 else alert('no');
+*/
 
+/*alert(5 / u);*/
 
-alert(5 / u);
+//alert(5);
 
-alert(5);
+// string
+// number
+// boolean
+// undefined
+// Null null 
+// object
 
---
+++ Participants/Kennadiy/js/script1.js
@@ -0,0 +1,28 @@
+//'strict mode'
+
+/*let questionText = []; 
+    let answerFirst = [];
+    let answerSecond = [];
+    let answerThird = [];
+    let answerFourth = [];
+    let rightAnswer = [];
+
+
+    questionText[0] = "Какое из этих соединений использовалось в фототехнике?"
+    answerFirst[0] = "a) Al2O3"
+    answerSecond[0] = "б) Na2S2O3"
+    answerThird[0] = "в) Na2SO4"
+    answerFourth[0] = "г) NaAl(SO4)2"
+    rightAnswer[0] = "б"*/
+
+    //if (.right_answer) {alert('eeee boi')}
+
+    ask("Выпустить птичку?", "да", fly, die);
+
+function fly() {
+  alert( 'улетела :)' );
+}
+
+function die() {
+  alert( 'птичку жалко :(' );
+}
