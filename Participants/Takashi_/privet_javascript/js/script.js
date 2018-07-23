870992f0a8c6e9f29ca86e149e9668f4be92f800
TakashiNKZ <40176218+TakashiNKZ@users.noreply.github.com>
2018-07-23 12:13:27 +0300

    Add files via upload

---
+++ Participants/Takashi_/privet_javascript/js/script.js
@@ -0,0 +1,20 @@
+    document.getElementById("Huiaga1").onclick = function() {
+           document.getElementById("Huiaga1").style.background="red";
+           setTimeout("alert('Игра окончена')", 500);
+          setTimeout("location.reload()", 1000); 
+    }
+    document.getElementById("Huiaga3").onclick = function() {
+        document.getElementById("Huiaga3").style.background="green";
+        setTimeout("alert('Раунд пройден!')", 500);
+        setTimeout("document.location.href = 'index2.html'", 500);
+ }
+ document.getElementById("Huiaga4").onclick = function() {
+    document.getElementById("Huiaga4").style.background="red";
+    setTimeout("alert('Игра окончена')", 500);
+          setTimeout("location.reload()", 1000); 
+ }
+    document.getElementById("Huiaga2").onclick = function() {
+        document.getElementById("Huiaga2").style.background="red";
+        setTimeout("alert('Игра окончена')", 500);
+          setTimeout("location.reload()", 1000); 
+    }
---
+++ Participants/Takashi_/privet_javascript/js/script4.js
@@ -0,0 +1,20 @@
+document.getElementById("Huiaga1").onclick = function() {
+    document.getElementById("Huiaga1").style.background="red";
+    setTimeout("alert('Игра окончена')", 500);
+    setTimeout("document.location.href = 'index.html'", 500); 
+}
+document.getElementById("Huiaga2").onclick = function() {
+ document.getElementById("Huiaga2").style.background="green";
+ setTimeout("alert('Раунд пройден!')", 500);
+ setTimeout("document.location.href = 'index5.html'", 500);
+}
+document.getElementById("Huiaga4").onclick = function() {
+document.getElementById("Huiaga4").style.background="red";
+setTimeout("alert('Игра окончена')", 500);
+setTimeout("document.location.href = 'index.html'", 500);  
+}
+document.getElementById("Huiaga3").onclick = function() {
+ document.getElementById("Huiaga3").style.background="red";
+ setTimeout("alert('Игра окончена')", 500);
+ setTimeout("document.location.href = 'index.html'", 500);  
+}
